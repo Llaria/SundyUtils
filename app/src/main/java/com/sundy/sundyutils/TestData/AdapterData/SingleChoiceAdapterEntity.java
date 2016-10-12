c9b203751537c92f6178e77fd0367ae1a3231951
sundi <sundistar@163.com>
2016-10-12 14:47:47 +0800

    DS

---
+++ app/src/main/java/com/sundy/sundyutils/TestData/AdapterData/SingleChoiceAdapterEntity.java
@@ -0,0 +1,34 @@
+package com.sundy.sundyutils.TestData.AdapterData;
+
+/**
+ * 单选ListView适配器的数据源
+ * Created by sundi on 2016/10/12.
+ */
+public class SingleChoiceAdapterEntity {
+    private String num ;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return "SingleChoiceAdapterEntity{" +
+                "num='" + num + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/sundy/sundyutils/adapter/SingleChoiceAdapter.java
@@ -0,0 +1,26 @@
+package com.sundy.sundyutils.adapter;
+
+import android.content.Context;
+
+import com.sundy.sundyutils.TestData.AdapterData.SingleChoiceAdapterEntity;
+import com.sundy.sundyutils.adapter.baseAdapter.AbsCommonAdapter;
+import com.sundy.sundyutils.adapter.baseAdapter.ViewHolder;
+
+import java.util.List;
+
+/**
+ * 可以单选的listView适配器
+ * Created by sundi on 2016/10/12.
+ */
+public class SingleChoiceAdapter extends AbsCommonAdapter<SingleChoiceAdapterEntity>{
+
+
+    public SingleChoiceAdapter(Context context, List<SingleChoiceAdapterEntity> datas, int itemLayoutId) {
+        super(context, datas, itemLayoutId);
+    }
+
+    @Override
+    public void convert(ViewHolder holder, SingleChoiceAdapterEntity data, int position) {
+
+    }
+}
